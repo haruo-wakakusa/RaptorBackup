182856bc582ce1ce86790e7c445aa6a00f7fe05b
若草春男(廣瀬佳典) <hirose.yoshinori@wine.plala.or.jp>
2023-02-22 21:01:33 +0900

    CountTest1テストを追加

--- CompressTests/HuffmanTableTest.cs
+++ CompressTests/HuffmanTableTest.cs
@@ -24,5 +24,24 @@ namespace CompressTests
                 Assert.AreEqual(0, t.GetCount(255));
             }
         }
+
+        [TestMethod]
+        public void CountTest1()
+        {
+            var t = new HuffmanTable(256);
+            t.Add(1);
+            t.Add(2);
+            t.Add(3);
+            t.Add(2);
+            t.Add(1);
+            Assert.AreEqual(0, t.GetCount(0));
+            Assert.AreEqual(2, t.GetCount(1));
+            Assert.AreEqual(2, t.GetCount(2));
+            Assert.AreEqual(1, t.GetCount(3));
+            for (int i = 4; i < 256; i++)
+            {
+                Assert.AreEqual(0, t.GetCount((byte)i));
+            }
+        }
     }
 }
