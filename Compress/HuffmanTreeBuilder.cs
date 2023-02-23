442c888a0812bb035f1e9a1452bea3cdd848653f
若草春男(廣瀬佳典) <hirose.yoshinori@wine.plala.or.jp>
2023-02-23 22:09:26 +0900

    HufffmanTreeBuilderクラスを作成(仕掛かり)

---
+++ Compress/HuffmanTreeBuilder.cs
@@ -0,0 +1,47 @@
+﻿using Compress;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compress.HuffmanTree
+{
+    public class HuffmanTreeBuilder
+    {
+        public readonly HuffmanNode[] nodes;
+        public uint idx = 0;
+
+        public HuffmanTreeBuilder(HuffmanNode[] nodes)
+        {
+            this.nodes = (HuffmanNode[])nodes.Clone();
+            Array.Sort(this.nodes, (a, b) => { return (int)(a.Count - b.Count); });
+        }
+
+        public bool Increment()
+        {
+            if (this.idx >= this.nodes.Length)
+            {
+                throw new NotImplementedException();
+            }
+
+            if (this.idx + 1 == this.nodes.Length)
+            {
+                return false;
+            }
+
+            var b = new HuffmanBranch(this.nodes[this.idx], this.nodes[this.idx + 1]);
+            this.idx += 1;
+            this.nodes[this.idx] = b;
+            var i = idx;
+            while (i + 1 < this.nodes.Length && this.nodes[i].Count > this.nodes[i + 1].Count)
+            {
+                var tmp = this.nodes[i];
+                this.nodes[i] = this.nodes[i + 1];
+                this.nodes[i + 1] = tmp;
+                i += 1;
+            }
+            return true;
+        }
+    }
+}
--- CompressTests/HuffmanTreeTest.cs
+++ CompressTests/HuffmanTreeTest.cs
@@ -21,7 +21,32 @@ namespace CompressTests
                 var n = new HuffmanBranch(new HuffmanLeaf(2, 1), new HuffmanLeaf(3, 4));
                 Assert.AreEqual("([2, 1], [3, 4])", n.ToString());
             }
+        }
 
+        [TestMethod]
+        public void HuffmanTreeBuilder_Initialization_Test()
+        {
+            var n1 = new HuffmanLeaf(31, 1);
+            var n2 = new HuffmanLeaf(22, 2);
+            var n3 = new HuffmanLeaf(13, 3);
+            {
+                var b = new HuffmanTreeBuilder(new HuffmanNode[] { n1, n2, n3 });
+                Assert.AreEqual(n1, b.nodes[0]);
+                Assert.AreEqual(n2, b.nodes[1]);
+                Assert.AreEqual(n3, b.nodes[2]);
+            }
+            {
+                var b = new HuffmanTreeBuilder(new HuffmanNode[] { n3, n2, n1 });
+                Assert.AreEqual(n1, b.nodes[0]);
+                Assert.AreEqual(n2, b.nodes[1]);
+                Assert.AreEqual(n3, b.nodes[2]);
+            }
+            {
+                var b = new HuffmanTreeBuilder(new HuffmanNode[] { n2, n3, n1 });
+                Assert.AreEqual(n1, b.nodes[0]);
+                Assert.AreEqual(n2, b.nodes[1]);
+                Assert.AreEqual(n3, b.nodes[2]);
+            }
         }
     }
 }
